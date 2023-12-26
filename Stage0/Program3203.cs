8fac7866a5a6a0c075e4a41e5d79db6dd462b56a
pinig <pinig50@gmail.com>
2023-12-27 01:27:41 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program3203.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome3203();
+        Welcome4818();
+        Console.ReadKey();
+    }
+
+    private static void Welcome3203()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter your name:");
+    }
+
+    static partial void Welcome4818();
+}
