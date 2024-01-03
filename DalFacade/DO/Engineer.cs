51d92d39b996fbea0f95e04e149c2af262a8ee92
Itzchak Ohana <ye0533143018@gmail.com>
2024-01-03 14:25:39 +0200

    add a new engineer

---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,27 @@
+﻿namespace DO;
+
+/// Engineer Entity represents a engineer with all its props
+public record Engineer
+
+
+(
+        int ID,
+        double Cost,
+        int Level,
+
+        string? Email = null,
+        string? Name = null
+
+)
+
+
+
+
+{
+    public Engineer() : this(0, 0, 0) { } //empty ctor for stage 3
+}
+
+
+
+
+///// Student Entity represents a 
